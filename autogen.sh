b2ac6eeb5daab6d2e77e4b24c3b48d847dee0bea
zhangheng <zhangheng@kylinos.cn>
2017-12-13 13:53:50 +0800

    update autogen.sh.

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,7 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="mate-settings-daemon"
+PKG_NAME="ukui-settings-daemon"
 
 (test -f $srcdir/configure.ac) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
