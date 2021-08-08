c06370e0ba45423bc57ded6c920c3f1c890a0825
sunjunhao <sunjunhao@kylinos.cn>
2021-08-08 16:37:41 +0800

    修复master分支无法运行其他分支的bug

--- plugins/media-keys/mediakey-manager.cpp
+++ plugins/media-keys/mediakey-manager.cpp
@@ -1211,11 +1211,11 @@ void MediaKeysManager::doTouchpadAction()
 
 void MediaKeysManager::doMicSoundAction()
 {
-//    bool mute;
+    bool mute;
 //    mute = mate_mixer_stream_control_get_mute (mInputControl);
 //    mate_mixer_stream_control_set_mute(mInputControl, !mute);
-//    mDeviceWindow->setAction ( mute ? "audio-input-microphone-high-symbolic" : "audio-input-microphone-muted-symbolic");
-//    mDeviceWindow->dialogShow();
+    mDeviceWindow->setAction ( mute ? "audio-input-microphone-high-symbolic" : "audio-input-microphone-muted-symbolic");
+    mDeviceWindow->dialogShow();
 }
 void MediaKeysManager::doSoundActionALSA(int keyType)
 {
