efd202a2ec2b920311589594336cfd9ad0c9bcb1
shangxiaoyang <shangxiaoyang@kylinos.cn>
2021-09-27 18:37:55 +0800

    功能：添加版权

--- plugins/sharing/sharing-plugin.cpp
+++ plugins/sharing/sharing-plugin.cpp
@@ -1,7 +1,7 @@
 /* -*- Mode: C++; indent-tabs-mode: nil; tab-width: 4 -*-
  * -*- coding: utf-8 -*-
  *
- * Copyright (C) 2020 KylinSoft Co., Ltd.
+ * Copyright (C) 2021 KylinSoft Co., Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
